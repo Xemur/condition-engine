f2cff8b8061422933712b9e4973fa569fdbb4e47
Xemur <chamsylabidi2@gmail.com>
2025-04-10 16:08:19 +0200

    Add Vitest configuration for testing and coverage

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      include: ["src/**/*.ts"],
+      exclude: ["src/index.ts", "src/types/index.ts"],
+      all: true,
+    },
+  },
+}); 
